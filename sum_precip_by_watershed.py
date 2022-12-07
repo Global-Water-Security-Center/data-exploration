a48b01fcd1b8f053499ed6fe0060f1c1e1b6345b
Rich Sharp <rich@therealspring.org>
2022-12-07 15:18:47 -0800

    fixing issue wih lat/lng flipping

--- sum_precip_by_watershed.py
+++ sum_precip_by_watershed.py
@@ -32,13 +32,13 @@ def main():
     gdm_dataset = gdm_dataset.rio.write_crs('epsg:4326')
     gdm_dataset['time'] = pandas.DatetimeIndex(
         gdm_dataset.time.str.decode('utf-8').astype('datetime64'))
-
-    # convert lon that goes from 0 to 360 to -180 to 180
-    gdm_dataset.coords['longitude'] = (
-        gdm_dataset.coords['longitude'] + 180) % 360 - 180
-    gdm_dataset = gdm_dataset.sortby(gdm_dataset.longitude)
     date_range = pandas.date_range(
         start=args.start_date, end=args.end_date, freq='MS')
+
+    # convert lon that goes from 0 to 360 to -180 to 180
+    # gdm_dataset.coords['longitude'] = (
+    #     gdm_dataset.coords['longitude'] + 180) % 360 - 180
+    # gdm_dataset = gdm_dataset.sortby(gdm_dataset.longitude)
 
     print(f'loading vector {args.path_to_watersheds}')
     vector = geopandas.read_file(args.path_to_watersheds)
@@ -50,41 +50,97 @@ def main():
         float(numpy.max(vector.bounds.maxy)),
         float(numpy.min(vector.bounds.miny)))
     # lng slice is from -180 to 180 so min first
-    lon_slice = slice(
-        float(numpy.min(vector.bounds.minx)),
-        float(numpy.max(vector.bounds.maxx)))
+    min_lon = float(numpy.min(vector.bounds.minx))
+    max_lon = float(numpy.max(vector.bounds.maxx))
 
-    print(
-        f'subsetting to the following:'
-        f'\n\tdate_range: {date_range}'
-        f'\n\tlat slice: {lat_slice}'
-        f'\n\tlon slice: {lon_slice}')
+    sum_per_time_list = []
+    sum_over_time_list = []
+    # this loop handles some serious slowdown when querying across the meridian
+    # and instead breaks it up into western/eastern hemisphere and merges at
+    # the end
+    for lon_slice in [slice(0, max_lon), slice(min_lon, 0)]:
+        if lon_slice == slice(0, 0):
+            # it doesn't overlap meridian
+            continue
 
-    local_slice = gdm_dataset.sel(
-        latitude=lat_slice,
-        longitude=lon_slice,
-        time=date_range).sum_tp_mm
+        active_slice = lon_slice
+        local_vector = vector
+        if lon_slice.start < 0:
+            # transform geometry 360 degrees
+            local_vector = local_vector.translate(xoff=360)
+            print(local_vector.bounds.minx)
+            active_slice = slice(360+lon_slice.start, 360)
 
-    vector_basename = os.path.splitext(
-        os.path.basename(args.path_to_watersheds))[0]
-    print(f'clipping subset to {vector_basename}')
+        print(
+            f'subsetting to the following:'
+            f'\n\tdate_range: {date_range}'
+            f'\n\tlat slice: {lat_slice}'
+            f'\n\tlon slice: {active_slice}')
 
-    local_slice = local_slice.rio.clip(vector.geometry.values)
-    local_slice = local_slice.sum(dim='time', skipna=True, min_count=1)
-    local_slice.plot()
+        local_slice = gdm_dataset.sel(
+            latitude=lat_slice,
+            longitude=active_slice,
+            time=date_range).sum_tp_mm
+        vector_basename = os.path.splitext(
+            os.path.basename(args.path_to_watersheds))[0]
+        print(f'clipping subset to {vector_basename}')
+        try:
+            local_slice = local_slice.rio.clip(local_vector.geometry.values)
+        except rioxarray.exceptions.NoDataInBounds:
+            print(f'no data in bounds for slice {lat_slice}/{lon_slice}')
+            continue
 
-    target_path = f"{vector_basename}_precip_sum_{args.start_date}_{args.end_date}.tif"
-    print(f'writing result to {target_path}')
+        if lon_slice.start < 0:
+            # transform back into -180, 180 so it can merge
+            local_slice.coords['longitude'] = (
+                local_slice.coords['longitude'] + 180) % 360 - 180
+            local_slice = local_slice.sortby(local_slice.longitude)
+
+        sum_per_time = local_slice.sum(
+            dim=['longitude', 'latitude'], skipna=True, min_count=1)
+        sum_per_time_list.append(sum_per_time)
+
+        sum_over_time = local_slice.sum(dim='time', skipna=True, min_count=1)
+        sum_over_time_list.append(sum_over_time)
+
+    target_base = f"{vector_basename}_precip_sum_{args.start_date}_{args.end_date}"
+    target_table_path = f"{target_base}.csv"
+
+    sum_per_time = sum(sum_per_time_list)
+    with open(target_table_path, 'w') as csv_table:
+        csv_table.write('time,sum_tp_mm\n')
+        for val in sum_per_time:
+            csv_table.write(f'{val.time.data},{val.data}\n')
+    # sum_over_time = sum_over_time_list[0]
+    # print(sum_over_time)
+    # sum_over_time.plot()
+
+    #sum_over_time = xarray.merge(sum_over_time_list)
+    sum_over_time = sum_over_time_list[0].combine_first(sum_over_time_list[1])
+    print(sum_over_time)
+    sum_over_time.plot()
+
+    target_raster_path = f"{target_base}.tif"
+    print(f'writing result to {target_raster_path}')
     # get exact coords for correct geotransform
-    lat_slice = gdm_dataset.sel(latitude=lat_slice).latitude
-    lon_slice = gdm_dataset.sel(longitude=lon_slice).longitude
+    # convert lon that goes from 0 to 360 to -180 to 180
+    # gdm_dataset.coords['longitude'] = (
+    #     gdm_dataset.coords['longitude'] + 180) % 360 - 180
+    # gdm_dataset = gdm_dataset.sortby(gdm_dataset.longitude)
+    print(sum_over_time)
+    lat_slice = sum_over_time.latitude
+    lon_slice = sum_over_time.longitude
+    print(lat_slice)
+    print(lon_slice)
     xres = float((lon_slice[-1] - lon_slice[0]) / len(lon_slice))
     yres = float((lat_slice[-1] - lat_slice[0]) / len(lat_slice))
     transform = Affine.translation(
         lon_slice[0], lat_slice[0]) * Affine.scale(xres, yres)
 
+    print(f'making raster with bounds\n\t{lat_slice}:{lon_slice}\n\t{xres}*{yres}')
+
     with rasterio.open(
-        target_path,
+        target_raster_path,
         mode="w",
         driver="GTiff",
         height=len(lat_slice),
@@ -98,7 +154,8 @@ def main():
             'tiled': 'YES',
             'COMPRESS': 'LZW',
             'PREDICTOR': 2}) as new_dataset:
-        new_dataset.write(local_slice, 1)
+        new_dataset.write(sum_over_time, 1)
+        #new_dataset.write(sum_over_time, 1)
 
     plt.show()
 
