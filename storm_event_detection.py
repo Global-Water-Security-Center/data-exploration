75ad832889e3be08e209f7fd22044918e461d2ba
Rich Sharp <richpsharp@gmail.com>
2022-12-16 15:50:22 -0800

    works

--- storm_event_detection.py
+++ storm_event_detection.py
@@ -1,67 +1,83 @@
-"""Script to analyze a time series to determine when a storm event happens.
+"""Sum precipitation in a watershed(s) over a given time period.
 
-The storm of April 11 dumped nearly 300mm of rain
-in just over 24 hours and became the deadliest to hit
-South Africa in recent history eclipsing one-day
-rainfall totals from previous disasters. Including the
-1987 Durban floods that brought on average
-225mm rainfall in 24 hours and killed as many as
-500 people and the 2019 floods with 165mm rainfall
-in 24 hours left at least 80 dead
+Usage:
+call like this:
 
+python sum_precip_by_watershed.py europe_basins 2021-03-01 2022-03-01
 
+Then it will query the AER dataset
+http://h2o-sandbox1.aer-aws-nonprod.net/thredds/dodsC/era5/daily-summary.nc
+for the normal_sum_tp_mm value over that time period and generates two things:
+1) a geotiff named after the watershed file (in this case
+   europe_basins_precip_sum_2021-03-01_2022-03-01.tif) passed in and the
+   date range that's a summation of all the precip per pixel over the time
+   period provided.
+2) a CSV of TOTAL sum per time snapshot with the same naming convention.
 """
-import glob
+import argparse
+import os
 
-import xarray
-import pandas
-import numpy
-import rioxarray
+import ee
+import geemap
+import geopandas
+import requests
 
-import matplotlib.pyplot as plt
-import numpy as np
-
-#threadd_url = 'http://h2o-sandbox1.aer-aws-nonprod.net/thredds/dodsC/era5/daily-summary.nc'
-threadd_url = 'http://h2o-sandbox1.aer-aws-nonprod.net/thredds/dodsC/era5/normal-prcp-temp.nc'
+ERA5_RESOLUTION_M = 27830
+ERA5_TOTAL_PRECIP_BAND_NAME = 'total_precipitation'
 
 def main():
-    """Entry point."""
-    nc_path_list = list(glob.glob(r'D:\repositories\wwf-sipa\rain_events/*.nc'))
-    precip_array = numpy.zeros(len(nc_path_list), dtype=float)
-    date_array = numpy.empty(len(nc_path_list), dtype='datetime64[ns]')
-    # print(date_array)
-    for index, nc_path in enumerate(nc_path_list):
-        print(index, len(nc_path_list))
-        era5_daily_summary = rioxarray.open_rasterio(nc_path)
-        print(era5_daily_summary[0].attrs)
+    parser = argparse.ArgumentParser(
+        description='Rain events by watershed in a time range.')
+    parser.add_argument(
+        'path_to_watersheds', help='Path to vector/shapefile of watersheds')
+    parser.add_argument(
+        'start_date', help='start date for summation (YYYY-MM-DD) format')
+    parser.add_argument(
+        'end_date', help='start date for summation (YYYY-MM-DD) format')
+    parser.add_argument(
+        '--authenticate', action='store_true',
+        help='Pass this flag if you need to reauthenticate with GEE')
+    parser.add_argument(
+        '--rain_event_threshold', default=0.1, type=float,
+        help='amount of rain (mm) in a day to count as a rain event')
+    args = parser.parse_args()
+
+    if args.authenticate:
+        ee.Authenticate()
         return
-        current_date = era5_daily_summary.time.values[0]
-        date_array[index] = current_date
-        precip_array[index] = numpy.max(era5_daily_summary.precip)
+    ee.Initialize()
 
-    numpy.save('precip.np', precip_array)
-    print('done')
-    # plt.plot_date(date_array, precip_array)
-    # plt.show()
+    # convert to GEE polygon
+    gp_poly = geopandas.read_file(args.path_to_watersheds).to_crs('EPSG:4326')
+    local_shapefile_path = '_local_ok_to_delete.shp'
+    gp_poly.to_file(local_shapefile_path)
+    gp_poly = None
+    ee_poly = geemap.shp_to_ee(local_shapefile_path)
 
-    #era5_daily_summary['time'] = pandas.DatetimeIndex(era5_daily_summary.time.str.decode('utf-8').astype('datetime64'))
-    #date_range = pandas.date_range(start=START_DATE, end=END_DATE, freq='MS')
-    #era5_daily_summary = era5_daily_summary.sel(time=date_range)
+    era5_daily_collection = ee.ImageCollection("ECMWF/ERA5/DAILY")
+    era5_daily_collection = era5_daily_collection.filterDate(
+        args.start_date, args.end_date)
+    era5_daily_precip = era5_daily_collection.select(
+        ERA5_TOTAL_PRECIP_BAND_NAME).toBands().multiply(1000)  # convert to mm
+    era5_daily_precip = era5_daily_precip.where(
+        era5_daily_precip.lt(args.rain_event_threshold), 0).where(
+        era5_daily_precip.gte(args.rain_event_threshold), 1)
 
+    poly_mask = ee.Image.constant(1).clip(ee_poly).mask()
 
-    # vector = geopandas.read_file('europe_basins')
-    # vector = vector.to_crs({'init': 'epsg:4326'})
-    # print(vector.bounds.maxy)
-    # # lat slice goes from pos to neg because it's 90 to -90 degrees so max first
-    # lat_slice = slice(float(numpy.max(vector.bounds.maxy)), float(numpy.min(vector.bounds.miny)))
-    # # lng slice is from -180 to 180 so min first
-    # lon_slice = slice(float(numpy.min(vector.bounds.minx)), float(numpy.max(vector.bounds.maxx)))
-
-    # local_slice = era5_daily_summary.sel(
-    #     latitude=lat_slice,
-    #     longitude=lon_slice,
-    #     time=date_range)
-
+    era5_precip_event_sum = era5_daily_precip.reduce('sum').clip(
+        ee_poly).mask(poly_mask)
+    url = era5_precip_event_sum.getDownloadUrl({
+        'region': ee_poly.geometry().bounds(),
+        'scale': ERA5_RESOLUTION_M,
+        'format': 'GEO_TIFF'
+    })
+    response = requests.get(url)
+    vector_basename = os.path.basename(os.path.splitext(args.path_to_watersheds)[0])
+    precip_path = f"{vector_basename}_precip_events_{args.start_date}_{args.end_date}.tif"
+    print(f'calculate total precip event {precip_path}')
+    with open(precip_path, 'wb') as fd:
+        fd.write(response.content)
 
 
 if __name__ == '__main__':
