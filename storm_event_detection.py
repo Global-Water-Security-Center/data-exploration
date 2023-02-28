2c435db997f4aeb14a9b7ba63fad9dea8ca62e03
Rich Sharp <rich@therealspring.org>
2023-02-28 13:06:20 -0800

    stash

--- storm_event_detection.py
+++ storm_event_detection.py
@@ -15,6 +15,7 @@ for the normal_sum_tp_mm value over that time period and generates two things:
 2) a CSV of TOTAL sum per time snapshot with the same naming convention.
 """
 import argparse
+import datetime
 import os
 
 import ee
@@ -28,19 +29,10 @@ ERA5_TOTAL_PRECIP_BAND_NAME = 'total_precipitation'
 
 def main():
     parser = argparse.ArgumentParser(description=(
-        'Rain events by watershed in a time range.'
-        'Then it will query the AER dataset'
-        'http://h2o-sandbox1.aer-aws-nonprod.net/thredds/dodsC/era5/daily-summary.nc'
-        'for the normal_sum_tp_mm value over that time period and generates '
-        'two results:\n'
-        '1) a geotiff named after the watershed file (in this case\n'
-        '   europe_basins_precip_sum_{start date}-{end date}.tif) passed in '
-        'and the\n'
-        '   date range that\'s a summation of all the precip per pixel over \n'
-        'the time'
-        '   period provided.\n'
-        '2) a CSV of TOTAL sum per time snapshot with the same naming '
-        'convention.\n)'))
+        'Detect storm events in a 48 hour window using a threshold for '
+        'precip. Result is a geotiff raster whose pixels show the count of '
+        'detected rain events within a 48 hour period.'
+        ))
     parser.add_argument(
         'path_to_watersheds', help='Path to vector/shapefile of watersheds')
     parser.add_argument(
@@ -67,14 +59,30 @@ def main():
     gp_poly = None
     ee_poly = geemap.shp_to_ee(local_shapefile_path)
 
+    start_day = datetime.datetime.strptime(args.start_date, '%Y-%m-%d')
+    end_day = datetime.datetime.strptime(args.start_date, '%Y-%m-%d')
+
+    start_day_offset = start_day + datetime.timedelta(days=1)
+    end_day_offset = end_day + datetime.timedelta(days=1)
+
     era5_daily_collection = ee.ImageCollection("ECMWF/ERA5/DAILY")
-    era5_daily_collection = era5_daily_collection.filterDate(
-        args.start_date, args.end_date)
-    era5_daily_precip = era5_daily_collection.select(
-        ERA5_TOTAL_PRECIP_BAND_NAME).toBands().multiply(1000)  # convert to mm
-    era5_daily_precip = era5_daily_precip.where(
-        era5_daily_precip.lt(args.rain_event_threshold), 0).where(
-        era5_daily_precip.gte(args.rain_event_threshold), 1)
+
+    daily_precip_list = []
+    for working_start_day, working_end_day in [
+            (start_day, end_day), (start_day_offset, end_day_offset)]:
+        era5_daily_collection = era5_daily_collection.filterDate(
+            args.start_date, args.end_date)
+        # convert to mm and divide by 2 for average
+        era5_daily_precip = era5_daily_collection.select(
+            ERA5_TOTAL_PRECIP_BAND_NAME).toBands().multiply(1000/2)
+        daily_precip_list.append(era5_daily_precip)
+
+    average_24_precip = daily_precip_list[0].add(
+        daily_precip_list[1])
+
+    era5_daily_precip = average_24_precip.where(
+        average_24_precip.lt(args.rain_event_threshold), 0).where(
+        average_24_precip.gte(args.rain_event_threshold), 1)
 
     poly_mask = ee.Image.constant(1).clip(ee_poly).mask()
 
@@ -87,7 +95,7 @@ def main():
     })
     response = requests.get(url)
     vector_basename = os.path.basename(os.path.splitext(args.path_to_watersheds)[0])
-    precip_path = f'''{vector_basename}_24hr_precip_events_{
+    precip_path = f'''{vector_basename}_48hr_avg_precip_events_{
         args.start_date}_{args.end_date}.tif'''
     print(f'calculate total precip event {precip_path}')
     with open(precip_path, 'wb') as fd:
