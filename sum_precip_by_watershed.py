bb3890a765824f826c5fbbbe48a8bc356ed447b9
Rich Sharp <rich@therealspring.org>
2023-03-02 17:51:05 -0800

    fixed so we do ERA5 hourly

--- sum_precip_by_watershed.py
+++ sum_precip_by_watershed.py
@@ -2,6 +2,7 @@
 period."""
 import argparse
 import calendar
+import collections
 import datetime
 import hashlib
 import os
@@ -9,14 +10,13 @@ import os
 import ee
 import geemap
 import geopandas
+import numpy
 import requests
 
 ERA5_RESOLUTION_M = 27830
 ERA5_FILE_PREFIX = 'era5_monthly'
 ERA5_TOTAL_PRECIP_BAND_NAME = 'total_precipitation'
-ERA5_MEAN_AIR_TEMP_BAND_NAME = 'mean_2m_air_temperature'
-ERA5_BANDS_TO_REPORT = [
-    ERA5_TOTAL_PRECIP_BAND_NAME, ERA5_MEAN_AIR_TEMP_BAND_NAME]
+ERA5_AIR_TEMP_BAND_NAME = 'temperature_2m'
 CSV_BANDS_TO_DISPLAY = ['mean_precip (mm)', 'mean_2m_air_temp (C)']
 CSV_BANDS_SCALAR_CONVERSION = [
     lambda precip_m: precip_m*1000, lambda K_val: K_val-273.15]
@@ -24,26 +24,32 @@ ANNUAL_CSV_BANDS_SCALAR_CONVERSION = [
     lambda precip_m: precip_m*1000]
 
 
-def build_yearly_ranges(start_date, end_date):
+def build_monthly_ranges(start_date, end_date):
     start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
     end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
 
     current_day = start_date
     date_range_list = []
     while current_day < end_date:
+        current_year = current_day.year
+        current_month = current_day.month
         last_day = datetime.datetime(
-            year=current_day.year, month=12, day=31)
+            year=current_year, month=current_month,
+            day=calendar.monthrange(current_year, current_month)[1])
         last_day = min(last_day, end_date)
-        print(f'{current_day} -- {last_day}')
-        date_range_list.append((current_day, last_day))
+        date_range_list.append(
+            (current_day.strftime('%Y-%m-%d'),
+             last_day.strftime('%Y-%m-%d')))
+        # this kicks it to next month
         current_day = last_day+datetime.timedelta(days=1)
     return date_range_list
 
 
 def main():
     parser = argparse.ArgumentParser(description=(
-        'Sum annual precip and average annual temp by watershed in a time '
-        'range.'))
+        'Given a region and a time period, create two tables showing monthly '
+        'precip and mean temporature and another table showing annual '
+        'rainfall.'))
     parser.add_argument(
         'path_to_watersheds', help='Path to vector/shapefile of watersheds')
     parser.add_argument(
@@ -55,9 +61,9 @@ def main():
         help='Pass this flag if you need to reauthenticate with GEE')
     args = parser.parse_args()
 
-    yearly_date_range_list = build_yearly_ranges(
+    monthly_date_range_list = build_monthly_ranges(
         args.start_date, args.end_date)
-    return
+    print(monthly_date_range_list)
 
     if args.authenticate:
         ee.Authenticate()
@@ -78,45 +84,61 @@ def main():
     era5_hourly_collection = ee.ImageCollection("ECMWF/ERA5/HOURLY")
     era5_hourly_precip_collection = era5_hourly_collection.select(
         ERA5_TOTAL_PRECIP_BAND_NAME)
-
-    monthly_precip = ee.List([
+    monthly_precip_sum_list = ee.List([
         era5_hourly_precip_collection.filterDate(
             start_date, end_date).sum()
-        for start_date, end_date in yearly_date_range_list])
+        for start_date, end_date in monthly_date_range_list])
+
+    era5_hourly_temp_collection = era5_hourly_collection.select(
+        ERA5_AIR_TEMP_BAND_NAME)
+    monthly_mean_temp_list = ee.List([
+        era5_hourly_temp_collection.filterDate(
+            start_date, end_date).mean()
+        for start_date, end_date in monthly_date_range_list])
 
     poly_mask = ee.Image.constant(1).clip(ee_poly).mask()
 
-    def clip_and_sum(image, list_so_far):
-        list_so_far = ee.List(list_so_far)
-        reduced_dict = image.clip(ee_poly).mask(
-            poly_mask).reduceRegion(**{
-                'geometry': ee_poly,
-                'reducer': ee.Reducer.mean()
-            })
-        list_so_far = list_so_far.add(
-            [image.get(key) for key in ['year', 'month']] +
-            [reduced_dict.get(key) for key in ERA5_BANDS_TO_REPORT])
+    def clip_and_mean(band_id):
+        def _clip_and_mean(image, list_so_far):
+            list_so_far = ee.List(list_so_far)
+            reduced_dict = ee.Image(image).clip(ee_poly).mask(
+                poly_mask).reduceRegion(**{
+                    'geometry': ee_poly,
+                    'reducer': ee.Reducer.mean()
+                })
+            list_so_far = list_so_far.add(reduced_dict.get(band_id))
+            return list_so_far
+        return _clip_and_mean
 
-        return list_so_far
-    mean_per_band = monthly_precip.iterate(clip_and_sum, ee.List([]))
-    print()
+    # reduce the images down to a single value per ee_poly via mean
+    clipped_reduced_monthly_precip_mean_list = monthly_precip_sum_list.iterate(
+        clip_and_mean(ERA5_TOTAL_PRECIP_BAND_NAME), ee.List([]))
+    clipped_reduced_monthly_temp_mean_list = monthly_mean_temp_list.iterate(
+        clip_and_mean(ERA5_AIR_TEMP_BAND_NAME), ee.List([]))
 
     vector_basename = os.path.splitext(
         os.path.basename(args.path_to_watersheds))[0]
     target_base = f"{vector_basename}_monthly_precip_temp_mean_{args.start_date}_{args.end_date}"
     target_table_path = f"{target_base}.csv"
     print(f'generating summary table to {target_table_path}')
+    precip_by_year = collections.defaultdict(list)
     with open(target_table_path, 'w') as table_file:
         table_file.write('date,' + ','.join(CSV_BANDS_TO_DISPLAY) + '\n')
-        for payload in mean_per_band.getInfo():
+        for precip_mean, temp_mean, date in zip(
+                clipped_reduced_monthly_precip_mean_list.getInfo(),
+                clipped_reduced_monthly_temp_mean_list.getInfo(),
+                monthly_date_range_list):
+            year = date[0][:4]
+            converted_precip, converted_temp = [
+                _conv(x) for x, _conv in zip(
+                    (precip_mean, temp_mean),
+                    CSV_BANDS_SCALAR_CONVERSION)]
+            precip_by_year[year].append(converted_precip)
             table_file.write(
-                f'{payload[0]}-{payload[1]:02d},' +
-                ','.join([str(_conv(x)) for x, _conv in
-                          zip(payload[2:], CSV_BANDS_SCALAR_CONVERSION)]) +
-                '\n')
+                f'{date[0][:7]},{converted_precip},{converted_temp}\n')
 
-    era5_monthly_precp_collection = era5_hourly_collection.select(
-        ERA5_TOTAL_PRECIP_BAND_NAME).toBands()
+    era5_monthly_precp_collection = ee.ImageCollection(
+        monthly_precip_sum_list).toBands()
     era5_precip_sum = era5_monthly_precp_collection.reduce('sum').clip(
         ee_poly).mask(poly_mask).multiply(1000)
     url = era5_precip_sum.getDownloadUrl({
@@ -130,8 +152,8 @@ def main():
     with open(precip_path, 'wb') as fd:
         fd.write(response.content)
 
-    era5_monthly_temp_collection = era5_hourly_collection.select(
-        ERA5_MEAN_AIR_TEMP_BAND_NAME).toBands()
+    era5_monthly_temp_collection = ee.ImageCollection(
+        monthly_mean_temp_list).toBands()
     era5_temp_mean = era5_monthly_temp_collection.reduce('mean').clip(
         ee_poly).mask(poly_mask).subtract(273.15)
     url = era5_temp_mean.getDownloadUrl({
@@ -151,24 +173,17 @@ def main():
     print(f'generating summary table to {target_table_path}')
     with open(target_table_path, 'w') as table_file:
         table_file.write(f'date,{CSV_BANDS_TO_DISPLAY[0]}\n')
-        previous_year = None
-        running_sum = 0
         total_sum = 0
-        n_months = 0
-        for payload in mean_per_band.getInfo():
-            year = payload[0]
-            if previous_year != year:
-                if previous_year is not None:
-                    table_file.write(
-                        f'{previous_year},'
-                        f'{CSV_BANDS_SCALAR_CONVERSION[0](running_sum)}\n')
-                previous_year = year
-                running_sum = 0
-            local_val = payload[2:][0]
-            running_sum += local_val
-            total_sum += local_val
-            n_months += 1
-        table_file.write(f'total annual mean,{CSV_BANDS_SCALAR_CONVERSION[0](total_sum/n_months*12)}\n')
+        total_months = 0
+        for year, precip_list in sorted(precip_by_year.items()):
+            yearly_sum = numpy.sum(precip_list)
+            yearly_months = len(precip_list)
+            total_sum += yearly_sum
+            total_months += yearly_months
+            table_file.write(
+                f'{year},{yearly_sum/yearly_months}\n')
+            table_file.write(
+                f'total annual mean (adjusted to 12 months),{total_sum/total_months*12}\n')
 
 
 if __name__ == '__main__':
