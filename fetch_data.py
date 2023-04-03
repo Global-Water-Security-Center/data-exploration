6a6e051f3d805c83377229dfc4326c6b42b1d172
Rich Sharp <richpsharp@gmail.com>
2023-04-03 09:53:47 -0700

    convert to spaces and initalize help from ini

--- fetch_data.py
+++ fetch_data.py
@@ -4,10 +4,21 @@ import configparser
 
 GLOBAL_INI_PATH = 'defaults.ini'
 
+
 def main():
-	global_config = configparser.ConfigParser(allow_no_value=True)
+    """Entry point."""
+    global_config = configparser.ConfigParser(allow_no_value=True)
     global_config.read(GLOBAL_INI_PATH)
-    global_config.read(scenario_config_path)
+    global_config = global_config['defaults']
+    available_commands = global_config['available_commands'].split(',')
+    available_data = global_config['available_data'].split(',')
+    parser = argparse.ArgumentParser(description='Data platform entry point')
+    parser.add_argument('command', help=(
+        'Command to execute, one of: ' + ', '.join(available_commands)))
+    parser.add_argument('dataset_id', help=(
+        'Dataset ID to operate on, one of: ' + ', '.join(available_data)))
+    args = parser.parse_args()
+
 
 if __name__ == '__main__':
-	main()
+    main()
