7b3356ecb59ed5c6fca62d80ad7efc1d91c07c90
Rich Sharp <rich@therealspring.org>
2023-04-20 12:03:32 -0700

    fixing issue with date hanging -

--- monthly_and_annual_precip_temp_in_watershed.py
+++ monthly_and_annual_precip_temp_in_watershed.py
@@ -32,54 +32,7 @@ VARIABLE_ID_LIST = ['sum_tp_mm', 'mean_t2m_c']
 MASK_NODATA = -9999
 
 ERA5_RESOLUTION_M = 11132
-ERA5_FILE_PREFIX = 'era5_monthly'
-ERA5_TOTAL_PRECIP_BAND_NAME = 'total_precipitation'
-ERA5_AIR_TEMP_BAND_NAME = 'temperature_2m'
 CSV_BANDS_TO_DISPLAY = ['mean_precip (mm)', 'mean_2m_air_temp (C)']
-CSV_BANDS_SCALAR_CONVERSION = [
-    lambda precip_m: precip_m*1000, lambda K_val: K_val-273.15]
-ANNUAL_CSV_BANDS_SCALAR_CONVERSION = [
-    lambda precip_m: precip_m*1000]
-
-
-def get_monthly_precip_temp_mean(path_to_ee_poly, start_date, end_date):
-    ee_poly = geemap.geojson_to_ee(path_to_ee_poly)
-    poly_mask = ee.Image.constant(1).clip(ee_poly).mask()
-
-    era5_hourly_collection = ee.ImageCollection("ECMWF/ERA5/HOURLY")
-    era5_hourly_precip_collection = era5_hourly_collection.select(
-        ERA5_TOTAL_PRECIP_BAND_NAME)
-    monthly_precip_sum = era5_hourly_precip_collection.filterDate(
-        start_date, end_date).sum()
-
-    era5_hourly_temp_collection = era5_hourly_collection.select(
-        ERA5_AIR_TEMP_BAND_NAME)
-    monthly_mean_temp = era5_hourly_temp_collection.filterDate(
-        start_date, end_date).mean()
-
-    def clip_and_mean(image):
-        clipped_image = ee.Image(image).clip(ee_poly).mask(
-            poly_mask)
-        reduced_dict = clipped_image.reduceRegion(**{
-                'geometry': ee_poly,
-                'scale': ERA5_RESOLUTION_M,
-                'reducer': ee.Reducer.mean()
-            })
-        return clipped_image, reduced_dict
-
-    # reduce the images down to a single value per ee_poly via mean
-    (clipped_reduced_monthly_precip_image,
-     clipped_reduced_monthly_precip_mean) = clip_and_mean(monthly_precip_sum)
-    (clipped_reduced_monthly_temp_image,
-     clipped_reduced_monthly_temp_mean) = clip_and_mean(monthly_mean_temp)
-
-    return (
-        clipped_reduced_monthly_precip_image,
-        clipped_reduced_monthly_temp_image,
-        clipped_reduced_monthly_precip_mean.getInfo()[
-            ERA5_TOTAL_PRECIP_BAND_NAME],
-        clipped_reduced_monthly_temp_mean.getInfo()[
-            ERA5_AIR_TEMP_BAND_NAME])
 
 
 def _sum_op(*array_list):
@@ -143,6 +96,7 @@ def main():
         workspace_dir, 'monthly_temp_rasters')
     os.makedirs(monthly_temp_dir, exist_ok=True)
 
+    monthly_mean_list = []
     for start_date, end_date in monthly_date_range_list:
         month_start_day = datetime.datetime.strptime(start_date, '%Y-%m-%d')
         month_end_day = datetime.datetime.strptime(end_date, '%Y-%m-%d')
@@ -150,7 +104,6 @@ def main():
         raster_list_set = {
             variable_id: collections.defaultdict(list)
             for variable_id in VARIABLE_ID_LIST}
-        monthly_mean_list = []
         for date in daterange(month_start_day, month_end_day):
             for variable_id in VARIABLE_ID_LIST:
                 date_str = date.strftime('%Y-%m-%d')
@@ -213,11 +166,9 @@ def main():
             task_name=f'mean of {month_temp_path}')
 
         year = start_date[:4]
-        month = start_date[5:8]
+        month = start_date[5:7]
         monthly_mean_list.append(
             (year, month, precip_mean_task, temp_mean_task))
-
-    task_graph.join()
 
     # create table that lists precip and temp means per month
     target_base = (
@@ -318,90 +269,7 @@ def main():
     task_graph.join()
     task_graph.close()
 
-
-    # generate total mean temp raster
-
     LOGGER.info(f'********** ALL DONE -> results are in {workspace_dir}')
-
-
-    # ee_poly = geemap.geojson_to_ee(local_shapefile_path)
-    # poly_mask = ee.Image.constant(1).clip(ee_poly).mask()
-    # os.remove(local_shapefile_path)
-
-    # era5_monthly_precp_collection = ee.ImageCollection(
-    #     precip_image_list).toBands()
-    # era5_precip_sum = era5_monthly_precp_collection.reduce('sum').clip(
-    #     ee_poly).mask(poly_mask).multiply(1000)
-    # url = era5_precip_sum.getDownloadUrl({
-    #     'region': ee_poly.geometry().bounds(),
-    #     'scale': ERA5_RESOLUTION_M,
-    #     'format': 'GEO_TIFF'
-    # })
-    # response = requests.get(url)
-    # precip_path = (
-    #     f"{vector_basename}_precip_mm_sum_"
-    #     f"{args.start_date}_{args.end_date}.tif")
-    # print(f'total precip sum raster: {precip_path}')
-    # with open(precip_path, 'wb') as fd:
-    #     fd.write(response.content)
-
-    # era5_monthly_temp_collection = ee.ImageCollection(
-    #     temp_image_list).toBands()
-    # era5_temp_mean = era5_monthly_temp_collection.reduce('mean').clip(
-    #     ee_poly).mask(poly_mask).subtract(273.15)
-    # url = era5_temp_mean.getDownloadUrl({
-    #     'region': ee_poly.geometry().bounds(),
-    #     'scale': ERA5_RESOLUTION_M,
-    #     'format': 'GEO_TIFF'
-    # })
-    # response = requests.get(url)
-    # temp_path = (
-    #     f"{vector_basename}_temp_C_monthly_mean_"
-    #     f"{args.start_date}_{args.end_date}.tif")
-    # print(f'mean temp raster: {temp_path}')
-    # with open(temp_path, 'wb') as fd:
-    #     fd.write(response.content)
-
-    # # get annual mean of precip
-    # target_base = (
-    #     f"{vector_basename}_annual_precip_mean_"
-    #     f"{args.start_date}_{args.end_date}")
-    # target_table_path = f"{target_base}.csv"
-    # print(f'generating summary table to {target_table_path}')
-    # with open(target_table_path, 'w') as table_file:
-    #     table_file.write(f'date,yearly sum of {CSV_BANDS_TO_DISPLAY[0]}\n')
-    #     total_sum = 0
-    #     total_months = 0
-    #     for year, precip_list in sorted(precip_by_year.items()):
-    #         yearly_sum = numpy.sum(precip_list)
-    #         yearly_months = len(precip_list)
-    #         total_sum += yearly_sum
-    #         total_months += yearly_months
-
-    #         table_file.write(
-    #             f'{year},{yearly_sum}\n')
-    #     table_file.write(
-    #         f'total annual mean (adjusted to 12 months),'
-    #         f'{total_sum/total_months*12}\n')
-
-    # # create a table for precip and temp that is by MONTH showing a mean
-    # # value for that month over every year in the time period
-    # for table_type, dict_by_month in [
-    #         ('precip', precip_by_month), ('temp', temp_by_month)]:
-    #     monthly_normal_table_path = (
-    #         f"{vector_basename}_monthly_{table_type}_normal_"
-    #         f"{args.start_date}_{args.end_date}.csv")
-    #     target_table_path = f"{target_base}.csv"
-    #     with open(monthly_normal_table_path, 'w') as \
-    #             monthly_normal_table:
-    #         monthly_normal_table.write(f'month,avg {table_type}\n')
-    #         for month_id, data_list in sorted(dict_by_month.items()):
-    #             monthly_normal_table.write(
-    #                 f'{month_id},{numpy.average(data_list)}\n')
-
-    #     print(
-    #         f'monthly {table_type} normal table at: '
-    #         f'{monthly_normal_table_path}')
 
 
 if __name__ == '__main__':
