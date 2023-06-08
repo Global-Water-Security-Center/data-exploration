3dfd301b50207e5b6d0eb224721d9225c48bed16
Rich Sharp <rich@therealspring.org>
2023-06-08 15:37:36 -0700

    refactor for a more usable netcat to geotiff

--- netcat_to_geotiff.py
+++ netcat_to_geotiff.py
@@ -3,8 +3,8 @@ import glob
 import os
 
 import argparse
-import pandas
 import numpy
+import itertools
 import rasterio
 from rasterio.transform import Affine
 import xarray
@@ -20,6 +20,8 @@ def main():
     parser.add_argument(
         'x_y_fields', nargs=2,
         help='the names of the x and y coordinates in the netcat file')
+    parser.add_argument('--band_field', help=(
+        'if defined, will use this coordinate as the band field'))
     parser.add_argument(
         '--target_nodata', type=float,
         help='Set this as target nodata value if desired')
@@ -52,14 +54,23 @@ def main():
                 (coord_array[-1] - coord_array[0]) / len(coord_array)))
             coord_list.append(coord_array)
 
-        print(dataset.coords)
-        if len(dataset.coords) == 3:
-            band_coord = set(dataset.coords).difference(args.x_y_fields).pop()
-            print(band_coord)
-            n_bands = len(dataset.coords[band_coord])
-        else:
-            n_bands = 1
-        print(f'expecting {n_bands} bands')
+        n_bands = 1
+        if args.band_field is not None:
+            n_bands = len(dataset.coords[args.band_field])
+
+        remaining_coords = (
+            set(dataset.coords) - set(args.x_y_fields+[args.band_field]))
+
+        # Reduce the dimensionality of the dataset using the coordinates
+        coord_values = {
+            coord_name: dataset[coord_name].values
+            for coord_name in remaining_coords
+        }
+
+        # get all combinations
+        combinations = list(itertools.product(*coord_values.values()))
+        if not combinations:
+            combinations = [None]
 
         transform = Affine.translation(
             *[a[0] for a in coord_list]) * Affine.scale(*res_list)
@@ -67,25 +78,44 @@ def main():
         basename = os.path.basename(os.path.splitext(nc_path)[0])
 
         for variable_name in dataset.keys():
-            target_dir = os.path.join(args.out_dir, variable_name)
-            os.makedirs(target_dir, exist_ok=True)
-            with rasterio.open(
-                os.path.join(target_dir, f"{basename}_{variable_name}.tif"),
-                mode="w",
-                driver="GTiff",
-                height=len(coord_list[1]),
-                width=len(coord_list[0]),
-                count=n_bands,
-                dtype=numpy.float32,
-                nodata=None,
-                crs="+proj=latlong",
-                transform=transform,
-                **{
-                    'tiled': 'YES',
-                    'COMPRESS': 'LZW',
-                    'PREDICTOR': 2}) as new_dataset:
+            combination_suffix = ''
+            local_dataset = dataset
 
-                new_dataset.write(dataset[variable_name])
+            for combination in combinations:
+                local_selector = {
+                    key: value
+                    for key, value in zip(coord_values.keys(), combination)}
+                local_dataset = dataset.sel(**local_selector)
+                combination_suffix = ''.join([
+                    f'{key}{value}' for key, value in local_selector.items()])
+                target_dir = os.path.join(args.out_dir, variable_name)
+                os.makedirs(target_dir, exist_ok=True)
+                filename = f"{basename}_{variable_name}{combination_suffix}.tif"
+                print(filename)
+                with rasterio.open(
+                    os.path.join(target_dir, filename),
+                    mode="w",
+                    driver="GTiff",
+                    height=len(coord_list[1]),
+                    width=len(coord_list[0]),
+                    count=n_bands,
+                    dtype=numpy.float32,
+                    nodata=None,
+                    crs="+proj=latlong",
+                    transform=transform,
+                    **{
+                        'tiled': 'YES',
+                        'COMPRESS': 'LZW',
+                        'PREDICTOR': 2}) as new_dataset:
+                    print(local_dataset[variable_name])
+                    local_variable_ds = local_dataset[variable_name]
+                    if len(local_variable_ds.dims) == 2:
+                        new_dataset.write(
+                            local_variable_ds.expand_dims('band'))
+                    else:
+                        new_dataset.write(local_variable_ds)
+
+                    #local_dataset = local_dataset[variable_name].expand_dims('band')
 
                 # gdm_dataset = gdm_dataset.sel(
                 #     time=pandas.date_range(start='2012-01-01', end='2022-03-01', freq='MS'))
