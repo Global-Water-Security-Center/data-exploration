06f24e09a1eda61da36199fb6b2bde4125c04e86
Rich Sharp <rich@therealspring.org>
2023-06-08 13:51:35 -0700

    handle undefined times

--- ncinfo.py
+++ ncinfo.py
@@ -9,8 +9,18 @@ def main():
         description='Dump netcat info on a file to command line.')
     parser.add_argument('raster_path', help='path to netcat file')
     args = parser.parse_args()
-    print(xarray.open_dataset(args.raster_path))
-    print(rioxarray.open_rasterio(args.raster_path))
+    decode_times = True
+    while True:
+        try:
+            print(rioxarray.open_rasterio(
+                args.raster_path, decode_times=decode_times))
+            print(xarray.open_dataset(
+                args.raster_path, decode_times=decode_times))
+            break
+        except ValueError:
+            if decode_times is False:
+                raise
+            decode_times = False
 
 
 if __name__ == '__main__':
