d92ef6f0c679ad77936de23d6a73575c36b9eb48
Rich Sharp <richpsharp@gmail.com>
2023-07-31 10:59:50 -0700

    fixing so there can be missing years

--- cmip6_yearly_total_precip_explorer.py
+++ cmip6_yearly_total_precip_explorer.py
@@ -6,6 +6,7 @@ import shutil
 import zipfile
 
 from osgeo import gdal
+import matplotlib.colors
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.lines as mlines
@@ -15,6 +16,13 @@ import numpy
 CACHE_DIR = '_cmip6_local_cache'
 LOCAL_WORKSPACE = r'D:\cmip6_workspace'
 os.makedirs(LOCAL_WORKSPACE, exist_ok=True)
+
+
+def line_seg(n, max_n):
+    dashes = [max_n]
+    for index, digit in enumerate(bin(n)[2:]):
+        dashes.append(1+2**index*int(digit))
+    return dashes
 
 
 def iterate_files(directory):
@@ -77,12 +85,19 @@ def main():
     args = parser.parse_args()
 
     variable_to_process = 'pr'
-    for scenario_to_process in ['historical', 'ssp245', 'ssp370']:
+    variant_set = set()
+    for scenario_to_process, start_year, end_year in [
+            ('historical', 1950, 2020),
+            ('ssp245', 2021, 2100),
+            ('ssp370', 2021, 2010)
+            ]:
         with ThreadPoolExecutor(4) as executor:
             model_index = {}
-            #models_to_run = []
+            variant_index = {}
             variant_count = collections.defaultdict(set)
             future_workers = {}
+            # TODO for debugging:
+            # models_to_run = []
             for file_path in iterate_files(CACHE_DIR):
                 _, _, variable, scenario, model, variant, _ = file_path.split(
                     os.path.sep)
@@ -90,77 +105,86 @@ def main():
                     continue
                 if scenario_to_process != scenario:
                     continue
-                if model not in model_index:
-                    model_index[model] = len(model_index)
-                # #for debugging, do one model
+                # # TODO: for debugging, do one model
                 # if len(models_to_run) < 2 and model not in models_to_run:
                 #     models_to_run.append(model)
                 # elif model not in models_to_run:
                 #     continue
                 zip_path = os.path.join(
                     LOCAL_WORKSPACE, os.path.basename(file_path))
-                year = zip_path[-8:-4]
-                # if year < '2000' or year > '2010':
-                #     continue
-                # if variant not in variant_count[model] and len(variant_count[model]) > 2:
-                #     continue
+                year = int(zip_path[-8:-4])
+                if year < start_year or year > end_year:
+                    continue
+                if model not in model_index:
+                    model_index[model] = len(model_index)
+                if variant not in variant_index:
+                    variant_index[variant] = len(variant_index)
+                variant_set.add(variant)
                 variant_count[model].add(variant)
                 future_workers[(year, scenario, model, variant)] = executor.submit(
                     process_file, file_path, zip_path, args.point)
 
         # at this point, all tasks are complete
-        model_to_variant_data = collections.defaultdict(list)
+        model_to_variant_data = collections.defaultdict(
+            lambda: [0]*(end_year-start_year+1))
+
         for index, ((year, scenario, model, variant), future) in enumerate(future_workers.items()):
             yearly_val = future.result()
             print(f'done with {index} of {len(future_workers)}')
-            model_to_variant_data[(scenario, model, variant)].append(
-                (year, yearly_val))
+            # this is a part i need to fix -- we might not get ALL the yearly
+            # values in it so we'll need to pre-allocate
+            model_to_variant_data[(model, variant)][
+                year-start_year] = yearly_val
         if len(model_to_variant_data) == 0:
             continue
 
-        cmap = plt.cm.get_cmap('tab20')
+        #cmap = plt.cm.get_cmap('tab20')
+        HSV_tuples = [
+            (x*1.0/len(model_index), 1.0, 1.0)
+            for x in range(len(model_index))]
+        color = [matplotlib.colors.hsv_to_rgb(x) for x in HSV_tuples]
 
         # Generate colors and line styles
-        color = [cmap(i % cmap.N) for i in range(len(model_index))]
-        linestyles = ['-', '--', ':', '-.'] * (len(model_index) // 4 + 1)
-
+        #color = [cmap(i % cmap.N) for i in range(len(model_index))]
+        linestyles = [
+            line_seg(i, len(variant_index)) for i in range(len(variant_index))]
         # create a new figure
         fig, ax = plt.subplots(2, figsize=(15, 15))
         labeled_models = set()
         variant_series = collections.defaultdict(list)
-        for (scenario, model, variant), yearly_val_list in model_to_variant_data.items():
+        year_list = list(range(start_year, end_year+1, 1))
+        for (model, variant), yearly_val_list in model_to_variant_data.items():
             # plot the data
             label = None
             if model not in labeled_models:
                 label = model
                 labeled_models.add(model)
-            dates, values = zip(*sorted(yearly_val_list))
-            ax[0].plot(
-                dates,
-                values,
+            line, = ax[0].plot(
+                year_list,
+                yearly_val_list,
                 label=label,
                 linewidth=1,
                 color=color[model_index[model]],
-                linestyle=linestyles[model_index[model]],
+                #linestyle=linestyles[variant_index[variant]],
                 alpha=0.5,
                 )
+            line.set_dashes(linestyles[variant_index[variant]])
 
             # format the ticks
             ax[0].xaxis.set_major_locator(mdates.DayLocator(interval=30))  # adjust interval for your needs
             ax[0].xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
 
-            variant_series[model].append((dates, values))
-        for model, date_value_list in variant_series.items():
+            variant_series[model].append(yearly_val_list)
+        model_list = []
+        for model, variant_series in variant_series.items():
             # plot mean and standard deviation
-            date_series, variant_series = zip(*date_value_list)
             mean = numpy.mean(variant_series, axis=0)
             std = numpy.std(variant_series, axis=0)
-            ax[1].plot(
-                date_series[0], mean,
-                color=color[model_index[model]],
-                linestyle=linestyles[model_index[model]])
+            line, ax[1].plot(
+                year_list, mean,
+                color=color[model_index[model]])
             ax[1].fill_between(
-                date_series[0], mean - std, mean + std,
+                year_list, mean - std, mean + std,
                 color=color[model_index[model]],
                 alpha=0.2)
 
@@ -179,9 +203,14 @@ def main():
         handles = [mlines.Line2D(
             [], [],
             color=color[model_index[model]],
-            linestyle=linestyles[model_index[model]],
             label=f'{model} ({len(variant_count[model])})')
             for model in model_index]
+
+        # for handle, (_, variant) in zip(handles, model_to_variant_data):
+        #     handle.set_dashes(linestyles[variant_index[variant]])
+
+        # for handle, variant in zip(handles, variant_index.values()):
+        #     handle.set_dashes(linestyles[variant_index[variant]])
         ax[0].legend(handles=handles)
 
         plt.tight_layout()
