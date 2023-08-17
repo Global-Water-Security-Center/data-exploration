93c6371492710b4aff5f6a1503c3df9f9b4b1232
Rich Sharp <rich@therealspring.org>
2023-08-17 13:38:33 -0700

    track better graph

--- pandas_visualization_example/pandas_visualization.py
+++ pandas_visualization_example/pandas_visualization.py
@@ -7,7 +7,7 @@ data = pd.read_csv('DP_LIVE_17082023222528203.csv')
 # Set the Year column as the index
 data.set_index('TIME', inplace=True)
 # Plot the data
-plt.figure(figsize=(10, 5))
+plt.figure(figsize=(10, 10))
 
 crop_types = set(data['SUBJECT'])
 country_ids = set(data['LOCATION'])
@@ -16,13 +16,13 @@ for crop_id in crop_types:
     crop_mask = data['SUBJECT']==crop_id
     for country_id in country_ids:
         country_mask = data['LOCATION'] == country_id
-        print(data[crop_mask & country_mask])
-        plt.plot(data[crop_mask & country_mask]['Value'], marker='o', label=f'{country_id} {crop_id}')
+        plt.plot(data[crop_mask & country_mask]['Value'], linewidth=0.1, marker='.', label=f'{country_id} {crop_id}')
 
 plt.xlabel('Year')
 plt.ylabel('Yield')
 plt.title('Crop Yield Over Time')
-plt.legend()
+plt.legend(ncols=4, fontsize='x-small')
+
 
 # Show the plot
 plt.show()
