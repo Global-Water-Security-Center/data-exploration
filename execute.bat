cc0739f1a39c5f9dd03b77c6527274beb0d853e2
Richard Sharp <richpsharp@gmail.com>
2023-09-01 12:21:01 -0700

    does a pull now too

--- execute.bat
+++ execute.bat
@@ -19,4 +19,4 @@ goto loop
 :endloop
 
 :: Run your Docker command with the cleaned arguments
-docker container prune -f && docker run -it -v "%CD%":/usr/local/gwsc_app therealspring/gwsc-executor:latest python3 !clean_args!
+docker container prune -f && docker pull therealspring/gwsc-executor:latest && docker run -it -v "%CD%":/usr/local/gwsc_app therealspring/gwsc-executor:latest python3 !clean_args!
