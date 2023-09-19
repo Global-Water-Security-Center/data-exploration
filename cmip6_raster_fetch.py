465fbdb367869c647e7d02c1c585b5c90f66603c
Richard Sharp <richpsharp@gmail.com>
2023-09-19 14:21:38 -0700

    refactoring to do all the models

--- cmip6_raster_fetch.py
+++ cmip6_raster_fetch.py
@@ -54,7 +54,8 @@ VALID_MODEL_LIST = [
 SSP_LIST = [285, 585]
 DATASET_ID = 'NASA/GDDP-CMIP6'
 DATASET_CRS = 'EPSG:4326'
-
+WORKSPACE_DIR = 'cmip6_raster_fetch_workspace'
+os.makedirs(WORKSPACE_DIR, exist_ok=True)
 
 def check_dataset_collection(model, dataset_id, band_id, start_year, end_year):
     def band_checker(image):
@@ -83,7 +84,9 @@ def check_dataset_collection(model, dataset_id, band_id, start_year, end_year):
         return False
 
 
-def download_geotiff(image, description, scale, ee_poly, clip_poly_path):
+def download_geotiff(
+        image, description, scale, ee_poly, clip_poly_path,
+        target_raster_path):
     url = image.resample('bilinear').getDownloadURL({
         'scale': scale,
         'crs': 'EPSG:4326',
@@ -97,7 +100,7 @@ def download_geotiff(image, description, scale, ee_poly, clip_poly_path):
         content_type = response.headers.get('content-type')
         if 'application/zip' in content_type or 'application/octet-stream' in content_type:
             zip_buffer = io.BytesIO(response.content)
-            target_preclip = f"pre_clip_{description}.tif"
+            target_preclip = os.path.join(WORKSPACE_DIR, f"pre_clip_{description}.tif")
 
             with zipfile.ZipFile(zip_buffer) as zip_ref:
                 zip_ref.extractall(f"{description}")
@@ -109,7 +112,6 @@ def download_geotiff(image, description, scale, ee_poly, clip_poly_path):
                             os.remove(target_preclip)
                         os.rename(f"{description}/{filename}", target_preclip)
                         print(f"Successfully downloaded and unzipped {filename}")
-            # TODO: warp w/ geoptorcesing
             r = gdal.OpenEx(target_preclip, gdal.OF_RASTER)
             b = r.GetRasterBand(1)
             b.SetNoDataValue(-9999)
@@ -117,7 +119,6 @@ def download_geotiff(image, description, scale, ee_poly, clip_poly_path):
             r = None
             raster_info = geoprocessing.get_raster_info(target_preclip)
 
-            target_raster_path = f'{description}.tif'
             geoprocessing.warp_raster(
                 target_preclip, raster_info['pixel_size'],
                 target_raster_path,
@@ -127,6 +128,7 @@ def download_geotiff(image, description, scale, ee_poly, clip_poly_path):
                     'all_touched': True,
                     })
             os.remove(target_preclip)
+            os.remove(f'{os.path.splitext(target_preclip)[0]}.xml')
             LOGGER.info(f'saved {target_raster_path}')
         else:
             print(f"Unexpected content type: {content_type}")
@@ -144,6 +146,8 @@ def main():
         help='Pass this flag if you need to reauthenticate with GEE')
     parser.add_argument(
         'aoi_vector_path', help='Path to vector/shapefile of area of interest')
+    parser.add_argument(
+        '--field_id_for_aggregate', help='Field ID in aoi for aggregating.')
     parser.add_argument('--where_statement', help=(
         'If provided, allows filtering by a field id and value of the form '
         'field_id=field_value'))
@@ -155,6 +159,8 @@ def main():
         'Two date ranges in YYYY format to download between.'))
     parser.add_argument(
         '--dataset_scale', type=float, help='Dataset scale', default=27830)
+    parser.add_argument(
+        '--table_path', help='Desired output table path.')
     args = parser.parse_args()
 
     gee_key_path = os.environ['GEE_KEY_PATH']
@@ -187,66 +193,83 @@ def main():
         filtered_models = list(
             filter(None, executor.map(filter_model, VALID_MODEL_LIST)))
 
-    cmip6_dataset = ee.ImageCollection(DATASET_ID).filter(
-        ee.Filter.And(
-            ee.Filter.inList('model', filtered_models),
-            ee.Filter.eq('scenario', args.scenario_id))).select(args.band_id)
+    if not os.path.exists(args.table_path):
+        make_header = True
+    table_file = open(args.table_path, 'a')
+    if make_header:
+        table_file.write('f{args.field_id_for_aggregate},band_id,scenario_id,model_id,aggregate_type,start-end-year,month,avg val in feature\n')
 
-    thread_list = []
-    for month in range(1, 13):
-        monthly_collection = cmip6_dataset.filter(
-            ee.Filter.calendarRange(month, month, 'month')).filter(
-            ee.Filter.calendarRange(start_year, end_year, 'year'))
-        if args.aggregate_type == 'min':
-            # convert to C
-            monthly_aggregate = monthly_collection.reduce(
-                ee.Reducer.min()).subtract(273.15)
-        elif args.aggregate_type.startswith('percentile'):
-            # convert to C
-            percentile = float(args.aggregate_type.split('_')[1])
-            monthly_aggregate = monthly_collection.reduce(
-                ee.Reducer.percentile([percentile])).subtract(273.15)
-        elif args.aggregate_type == 'sum':
-            # Group by model (replace 'model' with the actual property name)
-            unique_models = monthly_collection.aggregate_array(
-                'model').distinct()
+    models_to_analyze = [
+        (model_id, [model_id]) for model_id in filtered_models]
+    models_to_analyze += [('all_models', filtered_models)]
 
-            def reduce_by_model(model):
-                model_collection = monthly_collection.filter(
-                    ee.Filter.eq('model', model))
-                return model_collection.reduce(ee.Reducer.sum())
+    for model_id, model_list in models_to_analyze:
+        model_list = [model_id]
+        cmip6_dataset = ee.ImageCollection(DATASET_ID).filter(
+            ee.Filter.And(
+                ee.Filter.inList('model', model_list),
+                ee.Filter.eq('scenario', args.scenario_id))).select(
+            args.band_id)
 
-            # Reduce each model's images to a single image representing the
-            # sum over the time range
-            model_sums = unique_models.map(reduce_by_model)
+        thread_list = []
+        raster_path_map = {}
+        for month in range(1, 13):
+            description = (
+                f'{args.band_id}_{args.scenario_id}_{model_id}_monthly_{args.aggregate_type}'
+                f'{start_year}_{end_year}_{month}')
+            target_raster_path = os.path.join(WORKSPACE_DIR, f'{description}.tif')
+            raster_path_map[
+                (args.band_id, args.scenario_id, args.aggregate_type,
+                 f'{start_year}-{end_year}', month)] = target_raster_path
+            if os.path.exists(target_raster_path):
+                LOGGER.info(f'{target_raster_path} already exists, so skipping')
+                continue
 
-            # Convert to an ImageCollection and then reduce to a single image
-            # by taking the mean
-            model_sums_collection = ee.ImageCollection(model_sums)
-            monthly_aggregate = model_sums_collection.reduce(
-                ee.Reducer.mean()).divide((end_year-start_year+1)*30).multiply(
-                86400)
+            monthly_collection = cmip6_dataset.filter(
+                ee.Filter.calendarRange(month, month, 'month')).filter(
+                ee.Filter.calendarRange(start_year, end_year, 'year'))
+            if args.aggregate_type == 'min':
+                # convert to C
+                monthly_aggregate = monthly_collection.reduce(
+                    ee.Reducer.min()).subtract(273.15)
+            elif args.aggregate_type.startswith('percentile'):
+                # convert to C
+                percentile = float(args.aggregate_type.split('_')[1])
+                monthly_aggregate = monthly_collection.reduce(
+                    ee.Reducer.percentile([percentile])).subtract(273.15)
+            elif args.aggregate_type == 'sum':
+                # multiply by 86400 to convert to mm
+                monthly_aggregate = monthly_collection.reduce(
+                    ee.Reducer.sum()).multiply(
+                        86400/((end_year-start_year+1)*len(model_list)))
+            monthly_aggregate_clipped = monthly_aggregate.clip(ee_poly)
+            LOGGER.debug(monthly_aggregate_clipped.getInfo())
+            worker = threading.Thread(
+                target=download_geotiff,
+                args=(
+                    monthly_aggregate_clipped,
+                    description, args.dataset_scale, ee_poly,
+                    local_shapefile_path, target_raster_path))
+            worker.start()
+            thread_list.append(worker)
 
-            # multiply by 86400 to convert to mm
-            monthly_aggregate = monthly_collection.reduce(
-                ee.Reducer.sum()).divide(
-                (end_year-start_year+1)*30).multiply(86400)
-        monthly_aggregate_clipped = monthly_aggregate.clip(ee_poly)
-        LOGGER.debug(monthly_aggregate_clipped.getInfo())
-        description = (
-            f'{args.band_id}_{args.scenario_id}_monthly_{args.aggregate_type}'
-            f'{start_year}_{end_year}_{month}')
-        worker = threading.Thread(
-            target=download_geotiff,
-            args=(
-                monthly_aggregate_clipped,
-                description, args.dataset_scale, ee_poly, local_shapefile_path))
-        worker.start()
-        thread_list.append(worker)
+        for worker in thread_list:
+            worker.join()
 
-    for worker in thread_list:
-        worker.join()
+        aoi_vector = gdal.OpenEx(args.aoi_vector_path)
+        aoi_layer = aoi_vector.GetLayer()
+        for (band_id, scenario_id, aggregate_type, start_end_year, month), raster_path in raster_path_map.items():
+            zonal_stats_map = geoprocessing.zonal_statistics(
+                (raster_path, 1), args.aoi_vector_path,
+                polygons_might_overlap=False)
+            for fid, stats_map in zonal_stats_map.items():
+                feature = aoi_layer.GetFeature(fid)
+                aggregate_id = feature.GetField(args.field_id_for_aggregate)
+                val = (stats_map['max']+stats_map['min'])/2
+                table_file.write(f'{aggregate_id},{band_id},{scenario_id},{model_id},{aggregate_type},{start_end_year},{month},{val}\n')
+
     os.remove(local_shapefile_path)
+    table_file.close()
 
 
 if __name__ == '__main__':
