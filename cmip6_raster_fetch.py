a955b5afea7feb2fc5a01dc2c89447ce75aae1c7
Rich Sharp <richpsharp@gmail.com>
2023-09-21 14:23:11 -0700

    refactor for compressed result

--- cmip6_raster_fetch.py
+++ cmip6_raster_fetch.py
@@ -1,13 +1,14 @@
 """See `python scriptname.py --help"""
+from concurrent.futures import ThreadPoolExecutor
+from itertools import product
 import argparse
+import collections
 import io
 import logging
 import os
-import sys
 import shutil
+import sys
 import zipfile
-import threading
-from concurrent.futures import ThreadPoolExecutor
 
 from ecoshard import geoprocessing
 from osgeo import gdal
@@ -38,7 +39,6 @@ VALID_MODEL_LIST = [
     'FGOALS-g3',
     'GFDL-ESM4',
     'GISS-E2-1-G',
-    'SNRM-ESM2-1',
     # 'HadGEM3-GC31-MM',
     # 'IITM-ESM',
     # 'INM-CM5-0',
@@ -54,12 +54,15 @@ VALID_MODEL_LIST = [
     # 'UKESM1-0-LL',
 ]
 
-
+HISTORICAL_YEAR_CUTOFF = 2015  # anything less than 2015 is historical
 SSP_LIST = [285, 585]
 DATASET_ID = 'NASA/GDDP-CMIP6'
 DATASET_CRS = 'EPSG:4326'
 WORKSPACE_DIR = 'cmip6_raster_fetch_workspace'
 os.makedirs(WORKSPACE_DIR, exist_ok=True)
+VALID_VARIABLES = ['pr', 'tasmax', 'tasmin', 'tas']
+PRECIP_VARIABLES = {'pr'}
+TEMPERATURE_VARIABLES = {'tasmax', 'tasmin', 'tas'}
 
 def check_dataset_collection(model, dataset_id, band_id, start_year, end_year):
     def band_checker(image):
@@ -142,13 +145,45 @@ def download_geotiff(
         print(f"Failed to download {description} from {url}")
 
 
+def filter_by_julian_day(collection, start_year, end_year, julian_day, n_day_window):
+    years = ee.List.sequence(start_year, end_year)
+    initial = ee.ImageCollection([])
+
+    def year_filter(year, collection):
+        year = ee.Number(year)
+        start_date = ee.Date.fromYMD(year, 1, 1).advance(julian_day - 1, 'day')
+        start_window = start_date.advance(-n_day_window//2, 'day')
+        end_window = start_date.advance(n_day_window//2, 'day')
+
+        filtered = collection.filterDate(start_window, end_window)
+        return ee.ImageCollection(collection).merge(filtered)
+
+    return ee.ImageCollection(years.iterate(year_filter, initial))
+
+
+def authenticate():
+    try:
+        gee_key_path = os.environ['GEE_KEY_PATH']
+        credentials = ee.ServiceAccountCredentials(None, gee_key_path)
+        ee.Initialize(credentials)
+        return
+    except Exception:
+        pass
+
+    try:
+        ee.Initialize()
+        return
+    except Exception:
+        pass
+
+    ee.Authetnicate()
+    ee.Initialize()
+
+
 def main():
     parser = argparse.ArgumentParser(description=(
         'Fetch CMIP6 temperature and precipitation monthly normals given a '
         'year date range.'))
-    parser.add_argument(
-        '--authenticate', action='store_true',
-        help='Pass this flag if you need to reauthenticate with GEE')
     parser.add_argument(
         'aoi_vector_path', help='Path to vector/shapefile of area of interest')
     parser.add_argument(
@@ -156,21 +191,17 @@ def main():
     parser.add_argument('--where_statement', help=(
         'If provided, allows filtering by a field id and value of the form '
         'field_id=field_value'))
-    parser.add_argument('--aggregate_type', help='Either "sum" or "mean"')
-    parser.add_argument('--band_id', help="band id to fetch")
-    parser.add_argument(
-        '--scenario_id', help="Scenario ID ssp245, ssp585, historical")
-    parser.add_argument('--date_range', nargs=2, type=str, help=(
-        'Two date ranges in YYYY format to download between.'))
+    parser.add_argument('--date_range', nargs='+', type=str, help=(
+        'Date ranges in YYYY-YYYY format to download between.'))
     parser.add_argument(
         '--dataset_scale', type=float, help='Dataset scale', default=27830)
     parser.add_argument(
         '--table_path', help='Desired output table path.')
+    parser.add_argument(
+        '--n_day_window', default=10, type=int, help='Number of days in which to average around')
     args = parser.parse_args()
 
-    gee_key_path = os.environ['GEE_KEY_PATH']
-    credentials = ee.ServiceAccountCredentials(None, gee_key_path)
-    ee.Initialize(credentials)
+    authenticate()
 
     aoi_vector = geopandas.read_file(args.aoi_vector_path)
     if args.where_statement:
@@ -186,110 +217,117 @@ def main():
     aoi_vector = None
     ee_poly = geemap.geojson_to_ee(local_shapefile_path)
 
-    # Filter models dynamically based on data availability
-    start_year = int(args.date_range[0])
-    end_year = int(args.date_range[1])
+    raster_path_map = {}
+    executor = ThreadPoolExecutor(2 * os.cpu_count())
+    for date_range, variable_id in product(args.date_range, VALID_VARIABLES):
+        # Filter models dynamically based on data availability
+        start_year, end_year = [int(y) for y in date_range.split('-')]
 
-    def filter_model(model):
-        return model if check_dataset_collection(
-            model, DATASET_ID, args.band_id, start_year, end_year) else None
+        if end_year < HISTORICAL_YEAR_CUTOFF:
+            scenario_list = ['historical']
+        elif start_year >= HISTORICAL_YEAR_CUTOFF:
+            scenario_list = ['ssp245', 'ssp585']
+        else:
+            raise ValueError(
+                'start year and end year must be < 2015 for historical or '
+                f'>=2015 for scenarios, got this instead: {date_range}')
 
-    with ThreadPoolExecutor(len(VALID_MODEL_LIST)) as executor:
-        filtered_models = list(
+        def filter_model(model):
+            return model if check_dataset_collection(
+                model, DATASET_ID, variable_id, start_year, end_year) else None
+
+        valid_models = list(
             filter(None, executor.map(filter_model, VALID_MODEL_LIST)))
+        model_list_to_run = [('all_models', valid_models)]
 
-    make_header = not os.path.exists(args.table_path)
-    table_file = open(args.table_path, 'a')
-    if make_header:
-        table_file.write(f'{args.field_id_for_aggregate},band_id,scenario_id,model_id,aggregate_type,start-end-year,month,avg val in feature\n')
+        for (model_id, model_list), scenario_id in product(model_list_to_run, scenario_list):
+            cmip6_dataset = ee.ImageCollection(DATASET_ID).filter(
+                ee.Filter.And(
+                    ee.Filter.inList('model', model_list),
+                    ee.Filter.eq('scenario', scenario_id))).select(
+                variable_id)
 
-    models_to_analyze = []
-    # models_to_analyze += [
-    #     (model_id, [model_id]) for model_id in filtered_models]
-    models_to_analyze += [('all_models', filtered_models)]
+            for julian_day in [0]+list(range(args.n_day_window//2, 366, args.n_day_window)):
+                description = (
+                    f'{variable_id}_{scenario_id}_{model_id}_'
+                    f'{start_year}_{end_year}')
+                if julian_day > 0:
+                    # Convert Julian day to a Date object
+                    filtered_collection = filter_by_julian_day(
+                        cmip6_dataset, start_year, end_year, julian_day, args.n_day_window)
+                    description += f'_{julian_day:02d}'
+                else:
+                    description += '_year'
+                    filtered_collection = cmip6_dataset.filter(
+                        ee.Filter.calendarRange(start_year, end_year, 'year'))
 
-    for model_id, model_list in models_to_analyze:
-        cmip6_dataset = ee.ImageCollection(DATASET_ID).filter(
-            ee.Filter.And(
-                ee.Filter.inList('model', model_list),
-                ee.Filter.eq('scenario', args.scenario_id))).select(
-            args.band_id)
+                target_raster_path = os.path.join(WORKSPACE_DIR, f'{description}.tif')
+                if os.path.exists(target_raster_path):
+                    LOGGER.info(f'{target_raster_path} already exists, so skipping')
+                    raster_path_map[
+                        (variable_id, scenario_id, model_id,
+                         f'{start_year}-{end_year}', julian_day)] = target_raster_path
+                    continue
 
-        thread_list = []
-        raster_path_map = {}
-        for month in range(0, 13):
-            description = (
-                f'{args.band_id}_{args.scenario_id}_{model_id}_{args.aggregate_type}'
-                f'{start_year}_{end_year}')
-            filtered_collection = cmip6_dataset.filter(
-                ee.Filter.calendarRange(start_year, end_year, 'year'))
-            if month > 0:
-                filtered_collection = filtered_collection.filter(
-                    ee.Filter.calendarRange(month, month, 'month'))
-                description += f'_{month:02d}'
-            else:
-                description += '_year'
+                if variable_id in TEMPERATURE_VARIABLES:
+                    # convert to C
+                    aggregate = filtered_collection.reduce(
+                        ee.Reducer.mean()).subtract(273.15)
+                elif variable_id in PRECIP_VARIABLES:
+                    # multiply by 86400 to convert to mm
+                    aggregate = filtered_collection.reduce(
+                        ee.Reducer.sum()).multiply(
+                            86400/((end_year-start_year+1)*len(model_list)))
+                else:
+                    raise ValueError(
+                        f'unknown aggregate type for variable {variable_id}')
+                aggregate_clipped = aggregate.clip(ee_poly)
+                try:
+                    executor.submit(
+                        download_geotiff, aggregate_clipped, description,
+                        args.dataset_scale, ee_poly, local_shapefile_path,
+                        target_raster_path)
+                    raster_path_map[
+                        (variable_id, scenario_id, model_id,
+                         f'{start_year}-{end_year}', julian_day)] = target_raster_path
+                except Exception as e:
+                    LOGGER.error(f'***** ERROR on downloading geotiff, skipping: {e}')
 
-            target_raster_path = os.path.join(WORKSPACE_DIR, f'{description}.tif')
-            if os.path.exists(target_raster_path):
-                LOGGER.info(f'{target_raster_path} already exists, so skipping')
-                raster_path_map[
-                    (args.band_id, args.scenario_id, args.aggregate_type,
-                     f'{start_year}-{end_year}', month)] = target_raster_path
-                continue
+    executor.shutdown()
+    aoi_vector = gdal.OpenEx(args.aoi_vector_path)
+    aoi_layer = aoi_vector.GetLayer()
+    result_dict = collections.defaultdict(
+        lambda:collections.defaultdict(lambda: None))
+    scenario_key_set = set()
+    for (variable_id, scenario_id, model_id, start_end_year, julian_day), raster_path in \
+            raster_path_map.items():
+        if julian_day == 0:
+            julian_day = 'year'
+        zonal_working_dir = os.path.join(
+            WORKSPACE_DIR, os.path.basename(os.path.splitext(raster_path)[0]))
+        os.makedirs(zonal_working_dir, exist_ok=True)
+        zonal_stats_map = geoprocessing.zonal_statistics(
+            (raster_path, 1), args.aoi_vector_path,
+            polygons_might_overlap=False,
+            working_dir=zonal_working_dir)
+        shutil.rmtree(zonal_working_dir)
+        for fid, stats_map in zonal_stats_map.items():
+            feature = aoi_layer.GetFeature(fid)
+            aggregate_id = feature.GetField(args.field_id_for_aggregate)
+            val = (stats_map['max']+stats_map['min'])/2
+            scenario_key = f'{start_end_year} - {scenario_id}'
+            scenario_key_set.add(scenario_key)
+            result_dict[(aggregate_id, variable_id, model_id, julian_day)][scenario_key] = val
 
-            if args.aggregate_type == 'min':
-                # convert to C
-                aggregate = filtered_collection.reduce(
-                    ee.Reducer.min()).subtract(273.15)
-            elif args.aggregate_type.startswith('percentile'):
-                # convert to C
-                percentile = float(args.aggregate_type.split('_')[1])
-                aggregate = filtered_collection.reduce(
-                    ee.Reducer.percentile([percentile])).subtract(273.15)
-            elif args.aggregate_type == 'sum':
-                # multiply by 86400 to convert to mm
-                aggregate = filtered_collection.reduce(
-                    ee.Reducer.sum()).multiply(
-                        86400/((end_year-start_year+1)*len(model_list)))
-            aggregate_clipped = aggregate.clip(ee_poly)
-            try:
-                worker = threading.Thread(
-                    target=download_geotiff,
-                    args=(
-                        aggregate_clipped,
-                        description, args.dataset_scale, ee_poly,
-                        local_shapefile_path, target_raster_path))
-                worker.start()
-                thread_list.append(worker)
-                raster_path_map[
-                    (args.band_id, args.scenario_id, args.aggregate_type,
-                     f'{start_year}-{end_year}', month)] = target_raster_path
-            except Exception as e:
-                LOGGER.error(f'***** ERROR on downloading geotiff, skipping: {e}')
-
-        for worker in thread_list:
-            worker.join()
-
-        aoi_vector = gdal.OpenEx(args.aoi_vector_path)
-        aoi_layer = aoi_vector.GetLayer()
-        for (band_id, scenario_id, aggregate_type, start_end_year, month), raster_path in raster_path_map.items():
-            if month == 0:
-                month = 'year'
-            zonal_working_dir = os.path.join(WORKSPACE_DIR, os.path.basename(os.path.splitext(raster_path)[0]))
-            os.makedirs(zonal_working_dir, exist_ok=True)
-            zonal_stats_map = geoprocessing.zonal_statistics(
-                (raster_path, 1), args.aoi_vector_path,
-                polygons_might_overlap=False,
-                working_dir=zonal_working_dir)
-            shutil.rmtree(zonal_working_dir)
-            for fid, stats_map in zonal_stats_map.items():
-                feature = aoi_layer.GetFeature(fid)
-                aggregate_id = feature.GetField(args.field_id_for_aggregate)
-                val = (stats_map['max']+stats_map['min'])/2
-                table_file.write(f'{aggregate_id},{band_id},{scenario_id},{model_id},{aggregate_type},{start_end_year},{month},{val}\n')
-
+    scenario_key_list = list(sorted(scenario_key_set))
     os.remove(local_shapefile_path)
+    table_file = open(args.table_path, 'w')
+    table_file.write(f'{args.field_id_for_aggregate},variable,model ID,julian day,')
+    table_file.write(','.join(scenario_key_list) + '\n')
+    for (aggregate_id, variable_id, model_id, julian_day), val_dict in result_dict.items():
+        table_file.write(
+            f'{aggregate_id},{variable_id},{model_id},{julian_day},')
+        table_file.write(','.join(str(val_dict[key]) for key in scenario_key_list) + '\n')
     table_file.close()
 
 
