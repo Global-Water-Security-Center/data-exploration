ec87061e1ed79404d7d8e2b6e2fa8d54c3b98e72
Richard Sharp <richpsharp@gmail.com>
2023-10-03 19:31:51 -0700

    better naming

--- gwsc_fetch_mozambique.bat
+++ gwsc_fetch_mozambique.bat
@@ -4,26 +4,33 @@ set startyear=2035
 set endyear=2065
 
 for %%c in (120 221 321) do (
-    call python cmip6_fetch_tool.py --aoi_vector_path base_data/moz_regions --where_statement CR=%%c --scenario_id ssp245 --year_range %startyear% %endyear% --season_range 1 365 --variable_id pr --aggregate_function sum --eval_cmd 86400*var --target_table_path cmip6_output/ --file_prefix annual_total_precip_mm_
+    REM x Total annual precipitation
+    call python cmip6_fetch_tool.py --aoi_vector_path base_data/moz_regions --where_statement CR=%%c --scenario_id ssp245 --year_range %startyear% %endyear% --season_range 1 365 --variable_id pr --aggregate_function sum --eval_cmd 86400*var --target_table_path cmip6_output/ --file_prefix CR_annual_total_precip_mm_
 
-    call python cmip6_fetch_tool.py --aoi_vector_path base_data/moz_regions --where_statement CR=%%c --scenario_id ssp245 --year_range %startyear% %endyear% --season_range 1 365 --variable_id tas --aggregate_function mean --eval_cmd var-272.15 --target_table_path cmip6_output/ --file_prefix annual_mean_temp_c_
+    REM Mean annual temperature
+    call python cmip6_fetch_tool.py --aoi_vector_path base_data/moz_regions --where_statement CR=%%c --scenario_id ssp245 --year_range %startyear% %endyear% --season_range 1 365 --variable_id tas --aggregate_function mean --eval_cmd var-272.15 --target_table_path cmip6_output/ --file_prefix CR_annual_mean_temp_c_
 
+    REM Days Oct-Feb above 36ºC (Mozambique, maize)
     rem oct to feb is day 247 to feb 28 whch is day 59 so 365+59=424
-    call python cmip6_fetch_tool.py --aoi_vector_path base_data/moz_regions --where_statement CR=%%c --scenario_id ssp245 --year_range %startyear% %endyear% --season_range 247 424 --variable_id tas --aggregate_function gt_309.15 --target_table_path cmip6_output/ --file_prefix days_above_36c_oct_through_feb_
+    call python cmip6_fetch_tool.py --aoi_vector_path base_data/moz_regions --where_statement CR=%%c --scenario_id ssp245 --year_range %startyear% %endyear% --season_range 247 424 --variable_id tas --aggregate_function gt_309.15 --target_table_path cmip6_output/ --file_prefix CR_days_above_36c_oct_through_feb_
 )
 
 for %%c in (1 2 3 4) do (
-    call python cmip6_fetch_tool.py --aoi_vector_path base_data/HN_Koppen4Class.gpkg --where_statement KC=%%c --scenario_id ssp245 --year_range %startyear% %endyear% --season_range 1 365 --variable_id pr --aggregate_function sum --eval_cmd 86400*var --target_table_path cmip6_output/ --file_prefix annual_total_precip_mm_
+    REM x Total annual precipitation
+    call python cmip6_fetch_tool.py --aoi_vector_path base_data/HN_Koppen4Class.gpkg --where_statement KC=%%c --scenario_id ssp245 --year_range %startyear% %endyear% --season_range 1 365 --variable_id pr --aggregate_function sum --eval_cmd 86400*var --target_table_path cmip6_output/ --file_prefix HN_annual_total_precip_mm_
 
+    REM Total May-August precipitation (Honduras, Maize)
     REM 121 id may 1 243 is aug 31
-    call python cmip6_fetch_tool.py --aoi_vector_path base_data/HN_Koppen4Class.gpkg --where_statement KC=%%c --scenario_id ssp245 --year_range %startyear% %endyear% --season_range 121 243 --variable_id pr --aggregate_function sum --eval_cmd 86400*var --target_table_path cmip6_output/ --file_prefix total_precip_mm_may_through_aug
+    call python cmip6_fetch_tool.py --aoi_vector_path base_data/HN_Koppen4Class.gpkg --where_statement KC=%%c --scenario_id ssp245 --year_range %startyear% %endyear% --season_range 121 243 --variable_id pr --aggregate_function sum --eval_cmd 86400*var --target_table_path cmip6_output/ --file_prefix HN_total_precip_mm_may_through_aug
 
-    call python cmip6_fetch_tool.py --aoi_vector_path base_data/HN_Koppen4Class.gpkg --where_statement KC=%%c --scenario_id ssp245 --year_range %startyear% %endyear% --season_range 1 365 --variable_id tas --aggregate_function mean --eval_cmd var-272.15 --target_table_path cmip6_output/ --file_prefix annual_mean_temp_c_
+    REM Mean annual temperature
+    call python cmip6_fetch_tool.py --aoi_vector_path base_data/HN_Koppen4Class.gpkg --where_statement KC=%%c --scenario_id ssp245 --year_range %startyear% %endyear% --season_range 1 365 --variable_id tas --aggregate_function mean --eval_cmd var-272.15 --target_table_path cmip6_output/ --file_prefix HN_annual_mean_temp_c_
 
+    REM Days per year above 30 C (Honduras, Coffee)
     REM 303.15 is 30 deg C
-    call python cmip6_fetch_tool.py --aoi_vector_path base_data/HN_Koppen4Class.gpkg --where_statement KC=%%c --scenario_id ssp245 --year_range %startyear% %endyear% --season_range 1 365 --variable_id tas --aggregate_function gt_303.15 --target_table_path cmip6_output/ --file_prefix days_above_30c_
+    call python cmip6_fetch_tool.py --aoi_vector_path base_data/HN_Koppen4Class.gpkg --where_statement KC=%%c --scenario_id ssp245 --year_range %startyear% %endyear% --season_range 1 365 --variable_id tas --aggregate_function gt_303.15 --target_table_path cmip6_output/ --file_prefix HN_days_above_30c_
 
-    call python cmip6_fetch_tool.py --aoi_vector_path base_data/HN_Koppen4Class.gpkg --where_statement KC=%%c --scenario_id ssp245 --year_range %startyear% %endyear% --season_range 121 243 --variable_id tas --aggregate_function gt_309.15 --target_table_path cmip6_output/ --file_prefix days_above_36c_may_aug_
+    call python cmip6_fetch_tool.py --aoi_vector_path base_data/HN_Koppen4Class.gpkg --where_statement KC=%%c --scenario_id ssp245 --year_range %startyear% %endyear% --season_range 121 243 --variable_id tas --aggregate_function gt_309.15 --target_table_path cmip6_output/ --file_prefix HN_days_above_36c_may_aug_
 )
 
 REM For years: 2035-2065
